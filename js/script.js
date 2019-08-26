d8cf7751a1bf424fdb838afc83fcc97f7bd387f5
denzelwash <denzelwash33@mail.ru>
2019-08-26 23:16:19 +0300

    Сделал мобильную версию

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
+  // mobile-menu
+  var menuBtn = document.querySelector('.js-menu-btn');
+  var menu = document.querySelector('.page-header__nav');
+  menuBtn.addEventListener('click', function() {
+    this.classList.toggle('page-header__nav-btn--close');
+    menu.classList.toggle('page-header__nav--open');
+  });
+
 });
